a859c239ea9f57affc39ce1dc66a72025b3849a9
Muntean Sergiu <msergiu04@proton.me>
2025-12-05 14:20:38 +0200

    This reverts commit 5cb57ce7eacf42c40051c7877c32acca8c88d586.
    Client.Application/DTOs/UpdateTicketDto.cs #	Client.Web/Services/

--- Client.Web/Program.cs
+++ Client.Web/Program.cs
@@ -1,20 +1,36 @@
-﻿using Client.Web.Components;
-using MudBlazor;
-using MudBlazor.Services;
+using Client.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Client.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
-builder.Services.AddMudServices();
+            var app = builder.Build();
 
-var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-app.UseStaticFiles();
-app.UseAntiforgery();
+            app.UseHttpsRedirection();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+            app.UseAntiforgery();
 
-app.Run();
+            app.MapStaticAssets();
+            //app.MapRazorComponents<App>()
+            //    .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
